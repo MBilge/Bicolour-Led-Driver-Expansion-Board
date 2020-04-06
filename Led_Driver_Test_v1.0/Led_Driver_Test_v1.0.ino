5309f5f64c17b8432b4a1e103e412f4e09cc7ea1
MBILGE <m.selcukbilge@hotmail.com>
2020-04-06 16:15:53 +0100

    An expansion board for more pins or drive p to 16 bicolour/mono leds

---
+++ Led_Driver_Test_v1.0/Led_Driver_Test_v1.0.ino
@@ -0,0 +1,273 @@
+ 
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
+
+int t=100;
+
+Adafruit_MCP23017 LedDriver;
+ 
+typedef enum state_t {Green,Red,Off};
+
+void setup()
+{
+  Serial.begin(9600); // set up serial
+  Serial.println("Serial Started");
+  Wire.begin();           // Wire must be started!
+  
+  LedDriver.begin(0x27);  // Address set to zero.
+  AllPinOUTPUT();
+}
+
+void Led(int LedNumber,state_t state)
+{
+  switch (state){
+    case Green : LedDriver.pinMode(LedNumber,  OUTPUT);  // Make led available
+                 LedDriver.digitalWrite(LedNumber, HIGH);
+                 break;
+    case Red   : LedDriver.pinMode(LedNumber,  OUTPUT); // Make led available
+                 LedDriver.digitalWrite(LedNumber, LOW);
+                 break;            
+    case Off   : LedDriver.pinMode(LedNumber,  INPUT);
+                 break;
+  }
+}
+
+void AllPinOUTPUT()
+{
+  for (int pin=0;pin<16;pin++)
+  {
+    LedDriver.pinMode(pin,OUTPUT);
+  }
+}
+
+void AllPinINPUT() // will switch off all leds
+{
+  for (int pin=0;pin<16;pin++)
+  {
+    LedDriver.pinMode(pin,INPUT);
+  }
+}
+
+/*  Yapilacak showlar
+ * 6 - Walking green
+ * 7 - Walking red
+ * 8 - Mixing red/green/off
+ * 9 - 
+ * 10 -
+ */
+
+ void AllLedsGreen()
+ {
+  for (int leds=0 ; leds<16 ; leds++) Led(leds,Green);
+ }
+
+  void AllLedsRed()
+ {
+  for (int leds=0 ; leds<16 ; leds++) Led(leds,Red);
+ }
+
+   void AllLedsOff()
+ {
+  for (int leds=0 ; leds<16 ; leds++) Led(leds,Off);
+ }
+
+ void AllLedsGreenFlashing(int t,int r)
+ {
+  for (int i=0 ; i<r ; i++)
+  {
+    AllLedsGreen(); delay(t);
+    AllLedsOff();   delay(t);
+  }
+ }
+
+ void AllLedsRedFlashing(int t,int r)
+ {
+  for (int i=0 ; i<r ; i++)
+  {
+    AllLedsRed(); delay(t);
+    AllLedsOff(); delay(t);
+  }  
+ }
+
+ void AllLedsGreenAndRedFlashing(int t,int r)
+ {
+  for (int i=0 ; i<r ; i++)
+  { 
+    AllLedsGreen(); delay(t); 
+    AllLedsRed();   delay(t);
+  }   
+ }
+
+void WalkingGreenToRight(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsOff();
+    for (int i=0; i<16 ;i++)
+    {
+      Led(i,Green); delay(t);
+      Led(i,Off);   //delay(t);
+    }
+  }
+}
+
+void WalkingGreenToLeft(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsOff();
+    for (int i=15; i>=0 ;i--)
+    {
+      Led(i,Green); delay(t);
+      Led(i,Off);   //delay(t);
+    }
+  }
+}
+
+void WalkingRedToRight(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsOff();
+    for (int i=0; i<16 ;i++)
+    {
+      Led(i,Red); delay(t);
+      Led(i,Off);   //delay(t);
+    }
+  }
+}
+
+void WalkingRedToLeft(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsOff();
+    for (int i=15; i>=0 ;i--)
+    {
+      Led(i,Red); delay(t);
+      Led(i,Off);   //delay(t);
+    }
+  }
+}
+
+void FillingGreenToRight(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsOff();
+    for (int i=0; i<16 ;i++)
+    {
+      Led(i,Green); delay(t);
+    }
+  }
+}
+
+void FillingGreenToLeft(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsOff();
+    for (int i=15; i>=0 ;i--)
+    {
+      Led(i,Green); delay(t);
+    }
+  }
+}
+
+void FillingRedToRight(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsOff();
+    for (int i=0; i<16 ;i++)
+    {
+      Led(i,Red); delay(t);
+    }
+  }
+}
+
+void FillingRedToLeft(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsOff();
+    for (int i=15; i>=0 ;i--)
+    {
+      Led(i,Red); delay(t);
+    }
+  }
+}
+
+void UnFillingGreenToRight(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsGreen();
+    for (int i=15; i>=0 ;i--)
+    {
+      Led(i,Off); delay(t);
+    }
+  }
+}
+
+void UnFillingGreenToLeft(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsGreen();
+    for (int i=0; i<16 ;i++)
+    {
+      Led(i,Off); delay(t);
+    }
+  }
+}
+
+void UnFillingRedToRight(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsRed();
+    for (int i=15; i>=0 ;i--)
+    {
+      Led(i,Off); delay(t);
+    }
+  }
+}
+
+void UnFillingRedToLeft(int t,int r)
+{  
+  for (int n=0; n<r ; n++)
+  {
+    AllLedsRed();
+    for (int i=0; i<16 ;i++)
+    {
+      Led(i,Off); delay(t);
+    }
+  }
+}
+
+void loop()
+{
+  AllLedsOff(); // switch all leds off
+
+  AllLedsGreenFlashing(200,10);
+  AllLedsRedFlashing(200,10);
+  AllLedsGreenAndRedFlashing(200,10);
+
+  WalkingGreenToRight(100,1);
+  WalkingGreenToLeft(100,1);
+
+  WalkingRedToRight(100,1);
+  WalkingRedToLeft(100,1);  
+  
+  FillingGreenToRight(100,1);
+  UnFillingGreenToRight(100,1);
+  FillingGreenToLeft(100,1);
+  UnFillingGreenToLeft(100,1);
+  
+  FillingRedToRight(100,1);
+  UnFillingRedToRight(100,1);
+  FillingRedToLeft(100,1);
+  UnFillingRedToLeft(100,1);
+
+}
